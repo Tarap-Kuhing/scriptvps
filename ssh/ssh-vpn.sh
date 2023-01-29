8a773eadccce18848f93764edc91697b74c60acd
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-29 11:47:18 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -196,21 +196,21 @@ rm -f /etc/default/sslh
 
 # Settings SSLH
 cat > /etc/default/sslh <<-END
-# Default options for sslh initscript
-# sourced by /etc/init.d/sslh
+Default options for sslh initscript
+sourced by /etc/init.d/sslh
 
-# Disabled by default, to force yourself
-# to read the configuration:
-# - /usr/share/doc/sslh/README.Debian (quick start)
-# - /usr/share/doc/sslh/README, at "Configuration" section
-# - sslh(8) via "man sslh" for more configuration details.
-# Once configuration ready, you *must* set RUN to yes here
-# and try to start sslh (standalone mode only)
+Disabled by default, to force yourself
+to read the configuration:
+- /usr/share/doc/sslh/README.Debian (quick start)
+- /usr/share/doc/sslh/README, at "Configuration" section
+- sslh(8) via "man sslh" for more configuration details.
+Once configuration ready, you *must* set RUN to yes here
+and try to start sslh (standalone mode only)
 
 RUN=yes
 
-# binary to use: forked (sslh) or single-thread (sslh-select) version
-# systemd users: don't forget to modify /lib/systemd/system/sslh.service
+binary to use: forked (sslh) or single-thread (sslh-select) version
+systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
 
 DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
@@ -364,7 +364,7 @@ echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 # Install BBR
-wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+#wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 # Ganti Banner
 wget -O /etc/issue.net "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/issue.net"
