f4c10363a971b0cb6668b0b0480e5639cc826484
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-17 18:04:04 +0800

    Create update.sh

---
+++ update/update.sh
@@ -0,0 +1,270 @@
+#!/bin/bash
+###########- COLOR CODE -##############
+echo -e "\E[44;1;39m ⇱ [INFO] Downloading Update File⇲        \E[0m"
+sleep 1
+echo -e "\033[0;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[m"
+echo -e "\E[44;1;39m              ⇱ UPDATE ⇲                      \E[0m"
+echo -e "\E[44;1;39m              ⇱ DIMULAI ⇲                     \E[0m"
+echo -e "\033[0;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[m"
+clear
+#hapus menu
+rm -rf menu
+rm -rf portsshnontls
+rm -rf portsshws
+rm -rf sshovpnmenu
+rm -rf l2tpmenu
+rm -rf menu
+rm -rf pptpmenu
+rm -rf sstpmenu
+rm -rf wgmenu
+rm -rf ssmenu
+rm -rf ssrmenu
+rm -rf vmessmenu
+rm -rf vlessmenu
+rm -rf trmenu
+rm -rf trgomenu
+rm -rf setmenu
+rm -rf running
+rm -rf addhost
+rm -rf menu
+rm -rf addssh
+rm -rf trialssh
+rm -rf delssh
+rm -rf member
+rm -rf delexp
+rm -rf cekssh
+rm -rf restart
+rm -rf issue
+rm -rf speedtest
+rm -rf info
+rm -rf about
+rm -rf autokill
+rm -rf tendang
+rm -rf ceklim
+rm -rf ram
+rm -rf renewssh
+rm -rf clearlog
+rm -rf changeport
+rm -rf portovpn
+rm -rf portwg
+rm -rf porttrojan
+rm -rf portsstp
+rm -rf portsquid
+rm -rf portvlm
+rm -rf wbmn
+rm -rf xp
+rm -rf swapkvm
+rm -rf addvmess
+rm -rf addvless
+rm -rf addtrojan
+rm -rf delvmess
+rm -rf delvless
+rm -rf deltrojan
+rm -rf cekvmess
+rm -rf cekvless
+rm -rf cektrojan
+rm -rf renewvmess
+rm -rf renewvless
+rm -rf renewtrojan
+rm -rf certv2ray
+rm -rf addtrgo
+rm -rf deltrgo
+rm -rf renewtrgo
+rm -rf cektrgo
+rm -rf update
+
+# download menu
+cd /usr/bin
+rm -rf menu
+rm -rf portsshnontls
+rm -rf portsshws
+rm -rf sshovpnmenu
+rm -rf l2tpmenu
+rm -rf menu
+rm -rf pptpmenu
+rm -rf sstpmenu
+rm -rf wgmenu
+rm -rf ssmenu
+rm -rf ssrmenu
+rm -rf vmessmenu
+rm -rf vlessmenu
+rm -rf trmenu
+rm -rf trgomenu
+rm -rf setmenu
+rm -rf running
+rm -rf addhost
+rm -rf menu
+rm -rf addssh
+rm -rf trialssh
+rm -rf delssh
+rm -rf member
+rm -rf delexp
+rm -rf cekssh
+rm -rf restart
+rm -rf issue
+rm -rf speedtest
+rm -rf info
+rm -rf about
+rm -rf autokill
+rm -rf tendang
+rm -rf ceklim
+rm -rf ram
+rm -rf renewssh
+rm -rf clearlog
+rm -rf changeport
+rm -rf portovpn
+rm -rf portwg
+rm -rf porttrojan
+rm -rf portsstp
+rm -rf portsquid
+rm -rf portvlm
+rm -rf wbmn
+rm -rf xp
+rm -rf swapkvm
+rm -rf addvmess
+rm -rf addvless
+rm -rf addtrojan
+rm -rf delvmess
+rm -rf delvless
+rm -rf deltrojan
+rm -rf cekvmess
+rm -rf cekvless
+rm -rf cektrojan
+rm -rf renewvmess
+rm -rf renewvless
+rm -rf renewtrojan
+rm -rf certv2ray
+rm -rf addtrgo
+rm -rf deltrgo
+rm -rf renewtrgo
+rm -rf cektrgo
+rm -rf update
+
+wget -O /usr/bin/addhost "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/addhost.sh" && chmod +x /usr/bin/addhost
+wget -O /usr/bin/about "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/about.sh" && chmod +x /usr/bin/about
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/bin/addssh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/addssh.sh" && chmod +x /usr/bin/addssh
+wget -O /usr/bin/trialssh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/trialssh.sh" && chmod +x /usr/bin/trialssh
+wget -O /usr/bin/delssh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/delssh.sh" && chmod +x /usr/bin/delssh
+wget -O /usr/bin/member "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/member.sh" && chmod +x /usr/bin/member
+wget -O /usr/bin/delexp "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/delexp.sh" && chmod +x /usr/bin/delexp
+wget -O /usr/bin/cekssh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/cekssh.sh" && chmod +x /usr/bin/cekssh
+wget -O /usr/bin/restart "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/restart.sh" && chmod +x /usr/bin/restart
+wget -O /usr/bin/issue "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/issue.net" && chmod +x /usr/bin/issue
+wget -O /usr/bin/speedtest "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/speedtest_cli.py" && chmod +x /usr/bin/speedtest_cli
+wget -O /usr/bin/info "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/info.sh" && chmod +x /usr/bin/info
+wget -O /usr/bin/ram "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/ram.sh" && chmod +x /usr/bin/ram
+wget -O /usr/bin/renewssh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/renewssh.sh" && chmod +x /usr/bin/renewssh
+wget -O /usr/bin/autokill "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/autokill.sh" && chmod +x /usr/bin/autokill
+wget -O /usr/bin/ceklim "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/ceklim.sh" && chmod +x /usr/bin/ceklim
+wget -O /usr/bin/tendang "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/tendang.sh" && chmod +x /usr/bin/tendang
+wget -O /usr/bin/clearlog "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/clearlog.sh" && chmod +x /usr/bin/clearlog
+wget -O /usr/bin/changeport "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/changeport.sh" && chmod +x /usr/bin/changeport
+wget -O /usr/bin/portovpn "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/portovpn.sh" && chmod +x /usr/bin/portovpn
+wget -O /usr/bin/portwg "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/portwg.sh" && chmod +x /usr/bin/portwg
+wget -O /usr/bin/porttrojan "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/porttrojan.sh" && chmod +x /usr/bin/porttrojan
+wget -O /usr/bin/portsstp "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/portsstp.sh" && chmod +x /usr/bin/portsstp
+wget -O /usr/bin/portsquid "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/portsquid.sh" && chmod +x /usr/bin/portsquid
+wget -O /usr/bin/portvlm "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/portvlm.sh" && chmod +x /usr/bin/portvlm
+wget -O /usr/bin/wbmn "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/webmin.sh" && chmod +x /usr/bin/webmin
+wget -O /usr/bin/xp "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/xp.sh" && chmod +x /usr/bin/xp
+wget -O /usr/bin/swapkvm "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/swapkvm.sh" && chmod +x /usr/bin/swapkvm
+wget -O /usr/bin/addvmess "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/addv2ray.sh" && chmod +x /usr/bin/addv2ray
+wget -O /usr/bin/addvless "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/addvless.sh" && chmod +x /usr/bin/addvless
+wget -O /usr/bin/addtrojan "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/addtrojan.sh" && chmod +x /usr/bin/addtrojan
+wget -O /usr/bin/delvmess "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/delv2ray.sh" && chmod +x /usr/bin/delv2ray
+wget -O /usr/bin/delvless "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/delvless.sh" && chmod +x /usr/bin/delvless
+wget -O /usr/bin/deltrojan "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/deltrojan.sh" && chmod +x /usr/bin/deltrojan
+wget -O /usr/bin/cekvmess "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/cekv2ray.sh" && chmod +x /usr/bin/cekv2ray
+wget -O /usr/bin/cekvless "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/cekvless.sh" && chmod +x /usr/bin/cekvless
+wget -O /usr/bin/cektrojan "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/cektrojan.sh" && chmod +x /usr/bin/cektrojan
+wget -O /usr/bin/renewvmess "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/renewv2ray.sh" && chmod +x /usr/bin/renewv2ray
+wget -O /usr/bin/renewvless "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/renewvless.sh" && chmod +x /usr/bin/renewvless
+wget -O /usr/bin/renewtrojan "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/renewtrojan.sh" && chmod +x /usr/bin/renewtrojan
+wget -O /usr/bin/certv2ray "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/certv2ray.sh" && chmod +x /usr/bin/certv2ray
+wget -O /usr/bin/addtrgo "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/trojango/addtrgo.sh" && chmod +x /usr/bin/addtrgo
+wget -O /usr/bin/deltrgo "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/trojango/deltrgo.sh" && chmod +x /usr/bin/deltrgo
+wget -O /usr/bin/renewtrgo "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/trojango/renewtrgo.sh" && chmod +x /usr/bin/renewtrg
+wget -O /usr/bin/cektrgo "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/trojango/cektrgo.sh" && chmod +x /usr/bin/cektrgo
+wget -O /usr/bin/portsshnontls "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/portsshnontls.sh" && chmod +x /usr/bin/portsshnontls
+wget -O /usr/bin/portsshws "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/portsshws.sh" && chmod +x /usr/bin/portsshws
+wget -O /usr/bin/sshovpnmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/sshovpn.sh" && chmod +x /usr/bin/sshopn
+wget -O /usr/bin/l2tpmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/l2tpmenu.sh" && chmod +x /usr/bin/l2tpmenu
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/bin/pptpmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/pptpmenu.sh" && chmod +x /usr/bin/pptpmenu
+wget -O /usr/bin/sstpmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/sstpmenu.sh" && chmod +x /usr/bin/sstpmenu
+wget -O /usr/bin/wgmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/wgmenu.sh" && chmod +x /usr/bin/wgmenu
+wget -O /usr/bin/ssmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/ssmenu.sh" && chmod +x /usr/bin/ssmenu
+wget -O /usr/bin/ssrmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/ssrmenu.sh" && chmod +x /usr/bin/ssrmenu
+wget -O /usr/bin/vmessmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/vmessmenu.sh" && chmod +x /usr/bin/vmessmenu
+wget -O /usr/bin/vlessmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/vlessmenu.sh" && chmod +x /usr/bin/vlessmenu
+wget -O /usr/bin/trmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/trmenu.sh" && chmod +x /usr/bin/trmenu
+wget -O /usr/bin/trgomenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/trgomenu.sh" && chmod +x /usr/bin/trgomenu
+wget -O /usr/bin/setmenu "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/setmenu.sh" && chmod +x /usr/bin/setmenu
+wget -O /usr/bin/running "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/running.sh" && chmod +x /usr/bin/running
+wget -O /usr/bin/update "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/update/update.sh" && chmod +x /usr/bin/update
+
+chmod +x portsshnontls
+chmod +x portsshws
+chmod +x sshovpnmenu
+chmod +x l2tpmenu
+chmod +x menu
+chmod +x pptpmenu
+chmod +x sstpmenu
+chmod +x wgmenu
+chmod +x ssmenu
+chmod +x ssrmenu
+chmod +x vmessmenu
+chmod +x vlessmenu
+chmod +x trmenu
+chmod +x trgomenu
+chmod +x setmenu
+chmod +x running
+chmod +x addhost
+chmod +x menu
+chmod +x addssh
+chmod +x trialssh
+chmod +x delssh
+chmod +x member
+chmod +x delexp
+chmod +x cekssh
+chmod +x restart
+chmod +x issue
+chmod +x speedtest
+chmod +x info
+chmod +x about
+chmod +x autokill
+chmod +x tendang
+chmod +x ceklim
+chmod +x ram
+chmod +x renewssh
+chmod +x clearlog
+chmod +x changeport
+chmod +x portovpn
+chmod +x portwg
+chmod +x porttrojan
+chmod +x portsstp
+chmod +x portsquid
+chmod +x portvlm
+chmod +x wbmn
+chmod +x xp
+chmod +x swapkvm
+chmod +x addvmess
+chmod +x addvless
+chmod +x addtrojan
+chmod +x delvmess
+chmod +x delvless
+chmod +x deltrojan
+chmod +x cekvmess
+chmod +x cekvless
+chmod +x cektrojan
+chmod +x renewvmess
+chmod +x renewvless
+chmod +x renewtrojan
+chmod +x certv2ray
+chmod +x addtrgo
+chmod +x deltrgo
+chmod +x renewtrgo
+chmod +x cektrgo
+chmod +x update
+cd
