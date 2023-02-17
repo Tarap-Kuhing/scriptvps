18a9bc41b997ca19ed4ed510aea3f824ca0ec5d1
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-17 19:49:07 +0800

    Update cekvmess.sh

--- xray/cekvmess.sh
+++ xray/cekvmess.sh
@@ -27,7 +27,7 @@ exit 0
 fi
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vmess User Login ]=---------";
 echo "----------------------------------------";
