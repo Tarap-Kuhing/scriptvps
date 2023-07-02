204f8619a77bd962790d0ad12eb83a4a87a8df78
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-07-02 23:32:00 +0800

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -28,11 +28,8 @@ exit 0
 fi
 
 # Download File Ohp
-wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
-unzip ohpserver-linux32.zip
-chmod +x ohpserver
-cp ohpserver /usr/local/bin/ohpserver
-/bin/rm -rf ohpserver*
+wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/OPENVPN/ohp"
+chmod +x /usr/local/bin/ohp
 
 # Installing Service
 # SSH OHP Port 8181
